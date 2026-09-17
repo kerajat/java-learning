58a9d565a43bc3f0a59a321a641b01430ebb7750
kerajat <alfidev06@gmail.com>
2026-09-17 03:44:08 +0000

    day1

---
+++ week-01/Tugas1.java
@@ -0,0 +1,18 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+
+        int harga = 8500;
+        int jumlah = 3;
+
+        int total = harga * jumlah;
+
+        System.out.println("Harga:");
+        System.out.println(harga);
+
+        System.out.println("Jumlah:");
+        System.out.println(jumlah);
+
+        System.out.println("Total:");
+        System.out.println(total);
+    }
+}
